bb5453b00fbb7a629f62da5c87cf1624a5852423
Lagical <70884073+Lagical@users.noreply.github.com>
2023-08-09 15:07:59 +0300

    Update roll.js

--- roll.js
+++ roll.js
@@ -6,15 +6,20 @@ function getInputValue(){
     document.getElementById("max").innerHTML = maPrice;
 }
 
-function random() {
+  function random() {
     const miPrice = Math.abs(document.getElementById("miprice").value);
     const maPrice = Math.abs(document.getElementById("maprice").value);
     const chance = Math.floor(Math.random() * 100) + 1;
+    document.getElementById("price").innerHTML = Math.floor(Math.random() * (maPrice * 100 - miPrice * 100) + miPrice * 100) / 100;
     if (chance <= 50) {
-        document.getElementById("price").innerHTML = Math.floor(Math.random() * (maPrice * 100 - (miPrice) * 100) + miPrice * 100) / 100;
+        maPr = (30 / 100) * maPrice;
+        console.log("low");
+        document.getElementById("price").innerHTML = Math.floor(Math.random() * (maPr* 100 - miPrice * 100) + miPrice * 100) / 100;
     } else if (chance > 50 && chance <= 80){
-        document.getElementById("price").innerHTML = Math.floor(Math.random() * (maPrice * 100 - (miPrice) * 100) + miPrice * 100) / 100;
+        console.log("mid");
+        document.getElementById("price").innerHTML = Math.floor(Math.random() * ((60 / 100) * maPrice * 100 - ( 30 / 100) * maPrice * 100) + ( 30 / 100) * maPrice * 100) / 100;
     } else {
-        document.getElementById("price").innerHTML = Math.floor(Math.random() * (maPrice * 100 - (miPrice) * 100) + miPrice * 100) / 100;
+        console.log("high");
+        document.getElementById("price").innerHTML = Math.floor(Math.random() * (maPrice * 100 - ( 60 / 100) * maPrice * 100) + ( 60 / 100) * maPrice * 100) / 100;
     }
-  }
+}
