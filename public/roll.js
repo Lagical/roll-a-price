b29bb96b0d80578e203c4db36cade5e6382188ed
Lagical <70884073+Lagical@users.noreply.github.com>
2023-09-09 23:16:33 +0300

    history update
    roll history added

--- public/roll.js
+++ public/roll.js
@@ -7,19 +7,33 @@ function getInputValue(){
 }
 
   function random() {
+    let history= [];
     const miPrice = Math.abs(document.getElementById("miprice").value);
     const maPrice = Math.abs(document.getElementById("maprice").value);
     const maPr1 = (30 / 100) * maPrice;
     const maPr2 = (60 / 100) * maPrice;
     const chance = Math.floor(Math.random() * 100) + 1;
     if (chance <= 50) {
-        console.log("low");
-        document.getElementById("price").innerHTML = Math.floor(Math.random() * (maPr1 * 100 - miPrice * 100) + miPrice * 100) / 100 + " e";
+        const price = Math.floor(Math.random() * (maPr1 * 100 - miPrice * 100) + miPrice * 100) / 100 + " e";
+        document.getElementById("price").innerHTML = price;
+        history.push(price)
+        console.log(price + " low");
     } else if (chance > 50 && chance <= 80){
-        console.log("mid");
-        document.getElementById("price").innerHTML = Math.floor(Math.random() * (maPr2 * 100 - ( 30 / 100) * maPrice * 100) + maPr1 * 100) / 100 + " e";
+        const price = Math.floor(Math.random() * (maPr2 * 100 - ( 30 / 100) * maPrice * 100) + maPr1 * 100) / 100 + " e";
+        document.getElementById("price").innerHTML = price;
+        history.push(price)
+        console.log(price + " mid");
     } else {
-        console.log("high");
-        document.getElementById("price").innerHTML = Math.floor(Math.random() * (maPrice * 100 - maPr2 * 100) + maPr2 * 100) / 100 + " e";
+        const price = Math.floor(Math.random() * (maPrice * 100 - maPr2 * 100) + maPr2 * 100) / 100 + " e";
+        document.getElementById("price").innerHTML = price;
+        history.push(price)
+        console.log(price + " high");
     }
+        let list = document.getElementById("priceHistory");
+        for (i = 0; i < history.length; ++i) {
+            let li = document.createElement('li');
+            li.innerText = history[i];
+            list.appendChild(li);
+        }
+        console.log(history);
 }
