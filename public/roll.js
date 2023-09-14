a970aa5484bb69db07a499b93956c472292a1b0e
Lagical <70884073+Lagical@users.noreply.github.com>
2023-09-14 19:39:24 +0300

    some logic + styling

--- public/roll.js
+++ public/roll.js
@@ -10,24 +10,28 @@ function getInputValue(){
     let history= [];
     const miPrice = Math.abs(document.getElementById("miprice").value);
     const maPrice = Math.abs(document.getElementById("maprice").value);
-    const maPr1 = (30 / 100) * maPrice;
-    const maPr2 = (60 / 100) * maPrice;
+    const maPr1 = (30 / 100) * (maPrice-miPrice) + miPrice;
+    const maPr2 = (60 / 100) * (maPrice-miPrice) + miPrice;
     const chance = Math.floor(Math.random() * 100) + 1;
     if (chance <= 50) {
+        console.log(maPr1)
+        console.log(maPrice)
+        console.log(miPrice)
         const price = Math.floor(Math.random() * (maPr1 * 100 - miPrice * 100) + miPrice * 100) / 100 + " e";
-        document.getElementById("price").innerHTML = price;
+        console.log(price + " 1")
+        document.getElementById("price").innerHTML = "Current price " + price;
         history.push(price)
-        console.log(price + " low");
     } else if (chance > 50 && chance <= 80){
         const price = Math.floor(Math.random() * (maPr2 * 100 - ( 30 / 100) * maPrice * 100) + maPr1 * 100) / 100 + " e";
-        document.getElementById("price").innerHTML = price;
+        console.log(price + " 2")
+        console.log(maPr2)
+        document.getElementById("price").innerHTML = "Current price " + price;
         history.push(price)
-        console.log(price + " mid");
     } else {
         const price = Math.floor(Math.random() * (maPrice * 100 - maPr2 * 100) + maPr2 * 100) / 100 + " e";
-        document.getElementById("price").innerHTML = price;
+        console.log(price + " 3")
+        document.getElementById("price").innerHTML = "Current price " + price;
         history.push(price)
-        console.log(price + " high");
     }
         let list = document.getElementById("priceHistory");
         for (i = 0; i < history.length; ++i) {
